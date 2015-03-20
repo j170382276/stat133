cd5cfe401f25e23fba63e8adc2c11f76041aec24
j170382276 <weidongqin@WeidongdeMacBook-Air.local>
2015-03-20 16:22:22 -0700

    hw6

--- hw6/hw6.r
+++ hw6/hw6.r
@@ -28,17 +28,29 @@
 #                 non-adopter, else 1 (so once a row turns to 1 it stays as 1).
 
 sim.doctors <- function(initial.doctors, n.doctors, n.days, p){
-
+  has_adopted <- matrix(0, nrow= n.doctors, ncol = n.days)
+  has_adopted[,1] <- initial.doctors
   # Set up the output variable, define it as a matrix then use initial.doctors
   # to set the first column (day)
-
+  for(i in 2:n.days){
+    has_adopted[,i] <- has_adopted[, i-1]
+    docs <- sample(1:n.doctors, 2, replace = FALSE)
+    doc.one.adopt <- has_adopted[docs[1],i-1]
+    doc.two.adopt <- has_adopted[docs[2],i-1]
+    if(doc.one.adopt == 1 & doc.two.adopt == 0){
+      has_adopted[docs[2], i] <- sample(0:1, 1, replace = FALSE, prob = c(1-p,p))
+    }
+    if(doc.two.adopt == 1 & doc.one.adopt == 0){
+      has_adopted[docs[1], i] <- sample(0:1, 1, replace = FALSE, prob = c(1-p,p))
+    }
+  }
   # Run a simulation for <n.days> (use a for loop).  In the loop:
   # 1) pick two random doctors
   # 2) check if one has adopted the other hasn't
   # 3) convert the non-adopter with probability p
-
+  
   # return the output
-
+  return(has_adopted)
 }
 
 # When you test your function you have to generate <initial.doctors> and
@@ -46,7 +58,29 @@ sim.doctors <- function(initial.doctors, n.doctors, n.days, p){
 
 set.seed(42)
 # Generate a value for <initial.doctors> that has 10% 1s and 90% 0s.
+initial.doctors <- sample(0:1, 10, replace = TRUE, prob = c(0.9,0.1))
+
 # Run your function for at least 5 different values of <p> and plot
+
+trial1 <- sim.doctors(initial.doctors, n.doctors = 100, n.days=2500, p=0.1)
+plot(1:ncol(trial1), colSums(trial1),col="black",
+     type="l", xlab="Days", ylab="Number of Doctor's Adoption on the Drug",
+     main="Stimulation of adoption",
+     )
+
+trial2 <- sim.doctors(initial.doctors, n.doctors = 100, n.days=2500, p=0.2)
+lines(1:ncol(trial2), colSums(trial2),col="purple")
+
+trial3 <- sim.doctors(initial.doctors, n.doctors = 100, n.days=2500, p=0.3)
+lines(1:ncol(trial3), colSums(trial3), col="blue")
+
+trial4 <- sim.doctors(initial.doctors, n.doctors = 100, n.days=2500, p=0.4)
+lines(1:ncol(trial4), colSums(trial4), col="green")
+
+trial5 <- sim.doctors(initial.doctors, n.doctors = 100, n.days=2500, p=0.5)
+lines(1:ncol(trial5), colSums(trial5), col="red")
+
+
 # on x-axis: days,
 # on y-axis : the number of doctors that have already adopted the drug, on that day
 # Put all 5 lines in one figure (e.g. use first plot() then lines() for the subsequent lines)
